d4313916120d0a9062b4ed5c17e4c6b4646bdda2
Kevin Jin <lostking2008@hotmail.com>
2020-03-02 15:07:20 -0500

    Update IResolverProvider.cs
    add comments

--- ADOExt/Support/IResolverProvider.cs
+++ ADOExt/Support/IResolverProvider.cs
@@ -3,7 +3,15 @@
     public interface IResolverProvider
     {
         int Idx { get; }
+
+        /// <summary>
+        /// The full connection string, which should include user and password.
+        /// </summary>
         string ConnectionString { get; }
+
+        /// <summary>
+        /// The password of the connection string will be stripped away after the DBConnection is opened. The stripped connection string will be saved here.
+        /// </summary>
         string ConnectionStringOpened { get; }
         IDBClassResolver DBClassResolver { get; }
         ISqlResolver SqlResolver { get; }
